33c9ae547a2a02a0a41139e8f13439544fdd1b0f
Sabareesh Kappagantu <sbrsh@Sabareeshs-MacBook-Pro-2.local>
2020-09-24 16:37:47 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,22 @@
+// Section 1
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+
+app.get('/', (req, res) => { 
+ res.send("<h1>Home page</h1>");
+});
+
+app.get('/users', (req, res) => {
+    axios.get('https://randomuser.me/api/?page=1&results=10')
+     .then(response => {
+       res.send(response.data);
+     });
+   });
+
+app.listen(3000, () => {
+ console.log('server started on port 3000');
+});
---
+++ src/store/store.js
@@ -0,0 +1,9 @@
+import { createStore } from 'redux';
+import usersReducer from '../reducers/users';
+
+const store = createStore(usersReducer);
+
+store.subscribe(() => {
+ console.log('store data:', store.getState());
+});
+export default store;
