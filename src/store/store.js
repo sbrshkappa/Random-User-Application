6a8a39dc69bee75b5cb0d14efa2497fdd86ffd2f
Sabareesh Kappagantu <skappa@Sabareeshs-MacBook-Pro.local>
2020-09-26 16:54:37 +0530

    Chrome Redux Devtools Configuration

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,9 @@
-import { applyMiddleware, createStore } from 'redux';
+import { applyMiddleware, createStore, compose } from 'redux';
 import thunk from 'redux-thunk';
 import usersReducer from '../reducers/users';
 
-const store = createStore(usersReducer, applyMiddleware(thunk));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(usersReducer, composeEnhancers(applyMiddleware(thunk)));
 
 store.subscribe(() => {
  console.log('store data:', store.getState());
